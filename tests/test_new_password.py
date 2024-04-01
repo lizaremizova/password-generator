0b1a62bb1df7c419d99ce37e791dab88655a87e1
Kootyra <110038102+Kootyra@users.noreply.github.com>
2024-04-01 19:50:46 +0200

    first commit

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,17 +8,6 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-def test_password_length():
-    """Тест, что длина пароля соответствует заданной"""
-    for length in range(1, 21): 
-        assert len(generate_password(length)) == length
-
-def test_password_randomness():
-    """Тест, что два сгенерированных подряд пароля различаются"""
-    password1 = generate_password(10)
-    password2 = generate_password(10)
-    assert password1 != password2, "Два подряд сгенерированных пароля не должны быть одинаковыми."
-
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
