c250fae44a18a020fb58cfaa90d913a4530a4253
lizaremizova <er115@inbox.lv>
2024-09-21 16:41:48 +0300

    edited readme and comments in python files

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -2,16 +2,20 @@ import string
 from password.new_password import generate_password
 
 def test_password_characters():
-    """Тест, что при генерации используются только допустимые символы"""
+    """tests that generated password only contains valid characters"""
     valid_characters = string.ascii_letters + string.digits + string.punctuation
-    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    password = generate_password(100)  # generating a longer password for valid test
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
 
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_length_password():
+    """test that checks if passworg length is equal to required length"""
+    password = generate_password(12)
+    assert len(password) == 12
+
+def test_different_password():
+    """test that checks if two generated passwords are different"""
+    password1 = generate_password(12)
+    password2 = generate_password(12)
+    assert password1 != password2
